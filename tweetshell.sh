a5a19324cdec6faee1d8891d9aa4a73d8e3e13bd
rsdraju <64759679+rsdraju99@users.noreply.github.com>
2020-07-18 00:10:12 +0530

    Update tweetshell.sh

--- tweetshell.sh
+++ tweetshell.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Tweetshell v1.0
-# Coded by: github.com/thelinuxchoice
-# Instagram: @thelinuxchoice
+# Coded by: github.com/rsdraju99
 
 trap 'store;exit 1' 2
 
@@ -29,7 +28,7 @@ printf "\e[1;36m    _| || |_    | \ \ /\ / / _ \/ _ \ __\e[1;92m/ __| '_ \ / _ \
 printf "\e[1;36m   |_  __  _|   | |\ V  V /  __/  __/ |_\e[1;92m\__ \ | | |  __/ | | \e[0m\n"
 printf "\e[1;36m     |_||_|     \_/ \_/\_/ \___|\___|\__\e[1;92m|___/_| |_|\___|_|_| \e[0m\n"
 printf "\n"
-printf "\e[1;77m\e[44m Twitter BruteForcer v1.0 Author: thelinuxchoice (Github/IG)\e[0m\n"
+printf "\e[1;77m\e[44m Twitter BruteForcer v1.0 Author: rsdraju99      (Github/IG)\e[0m\n"
 printf "\n"
 }
 
